8b5c7b2678f25e4fc3aa321cdf033d7275bd14b9
Ancelotow <ancelotow@gmail.com>
2023-06-17 18:00:58 +0200

    .feat: filter hole 2 ✨

--- app/src/main/java/com/esgi/golf/application/score/ScoreActivity.kt
+++ app/src/main/java/com/esgi/golf/application/score/ScoreActivity.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.esgi.golf.R
 import com.esgi.golf.application.components.player_item.PlayerItemAdapter
 import com.esgi.golf.application.home.HomeViewModel
+import com.esgi.golf.domain.models.Game
 import com.esgi.golf.domain.models.Hole
 import com.esgi.golf.domain.models.Player
 import com.esgi.golf.domain.models.Round
@@ -23,26 +24,45 @@ import dagger.hilt.android.AndroidEntryPoint
 class ScoreActivity : AppCompatActivity() {
 
     private val viewModel: ScoreViewModel by viewModels()
-    var holeSelected: Hole? = null
+    private var holeSelected: Hole? = null
+    private var game: Game? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_score)
         supportActionBar?.hide()
 
+        val recyclerView: RecyclerView = findViewById(R.id.rv_players)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
         val spinner: Spinner = findViewById(R.id.hole_spinner)
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
                 holeSelected = spinner.selectedItem as Hole
-                viewModel.getGame()
+                recyclerView.adapter = PlayerItemAdapter(
+                    game!!.rounds.filter {
+                            e -> e.hole.id == (holeSelected?.id ?: 1)
+                    }.toMutableList(),
+                    ::addShot,
+                    ::removeShot,
+                    this@ScoreActivity
+                )
             }
 
             override fun onNothingSelected(parent: AdapterView<*>) {
-                holeSelected = null
+                recyclerView.adapter = PlayerItemAdapter(
+                    game!!.rounds.filter {
+                            e -> e.hole.id == (holeSelected?.id ?: 1)
+                    }.toMutableList(),
+                    ::addShot,
+                    ::removeShot,
+                    this@ScoreActivity
+                )
             }
         }
         val tvPlayer: TextView = findViewById(R.id.tv_player)
-        val recyclerView: RecyclerView = findViewById(R.id.rv_players)
+
+
         viewModel.gameState.observe(this) {
             when (it.status) {
                 ScoreStateStatus.Loading -> {
@@ -60,16 +80,15 @@ class ScoreActivity : AppCompatActivity() {
                     tvPlayer.text = getString(R.string.player_text, it.game.players.size.toString())
 
                     // RecyclerView
-                    recyclerView.layoutManager = LinearLayoutManager(this)
-                    recyclerView.adapter =
-                        PlayerItemAdapter(
-                            it.game.rounds.filter {
-                                    e -> e.hole.id == (holeSelected?.id ?: 1)
-                            }.toMutableList(),
-                            ::addShot,
-                            ::removeShot,
-                            this
-                        )
+                    game = it.game
+                    recyclerView.adapter = PlayerItemAdapter(
+                        it.game.rounds.filter {
+                                e -> e.hole.id == (holeSelected?.id ?: 1)
+                        }.toMutableList(),
+                        ::addShot,
+                        ::removeShot,
+                        this
+                    )
                 }
 
                 ScoreStateStatus.Error -> {
