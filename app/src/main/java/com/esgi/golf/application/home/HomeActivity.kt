c29b9913c3428f3172fa7df4112eaa640be134aa
Ancelotow <ancelotow@gmail.com>
2023-06-18 10:15:34 +0200

    .feat: add navigation on HomeActivity ✨

--- app/src/main/java/com/esgi/golf/application/home/HomeActivity.kt
+++ app/src/main/java/com/esgi/golf/application/home/HomeActivity.kt
@@ -1,18 +1,40 @@
 package com.esgi.golf.application.home
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Canvas
 import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.recyclerview.widget.RecyclerView
 import com.esgi.golf.R
+import com.esgi.golf.application.game.GameListActivity
+import com.esgi.golf.application.setup.GameSetupActivity
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var buttonSave: Button
+    private lateinit var buttonLoad: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         supportActionBar?.hide()
+        buttonSave = findViewById(R.id.button_new)
+        buttonLoad = findViewById(R.id.button_load)
+        buttonSave.setOnClickListener(::newGame)
+        buttonLoad.setOnClickListener(::loadGame)
+    }
+
+    private fun newGame(view: View) {
+        val intent = Intent(this, GameSetupActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun loadGame(view: View) {
+        val intent = Intent(this, GameListActivity::class.java)
+        startActivity(intent)
     }
 }
