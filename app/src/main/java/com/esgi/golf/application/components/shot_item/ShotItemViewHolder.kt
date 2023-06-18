04845dce544ca0cd327d964fece800c69eaea12c
Timoté Symetheus <timote.vannier@gmail.com>
2023-06-18 12:00:56 +0200

    GameReport update ui

--- app/src/main/java/com/esgi/golf/application/components/shot_item/ShotItemViewHolder.kt
+++ app/src/main/java/com/esgi/golf/application/components/shot_item/ShotItemViewHolder.kt
@@ -5,15 +5,22 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.esgi.golf.R
 import com.esgi.golf.domain.models.Round
+import android.content.Context
+
 
 class ShotItemViewHolder(v: View) : RecyclerView.ViewHolder(v) {
-
-    private val name = v.findViewById<TextView>(R.id.tv_player_name)
-    private val score = v.findViewById<TextView>(R.id.tv_player_score)
+    private val context: Context = v.context
+    private val roundOrder = v.findViewById<TextView>(R.id.tv_round_text)
+    private val shotsNumber = v.findViewById<TextView>(R.id.tv_shots_number)
+    private val hole = v.findViewById<TextView>(R.id.tv_hole_text)
 
     fun setItem(item: Round) {
-        "${item.player.firstname} ${item.player.name.uppercase()} ".also { name.text = it }
-        score.text = item.nbShot.toString()
+        // "${item.player.firstname} ${item.player.name.uppercase()} ".also { name.text = it }
+        // shotsNumber.text = item.nbShot.toString()
+
+        roundOrder.text = context.getString(R.string.round_order, item.order)
+        shotsNumber.text = context.getString(R.string.number_of_shots, item.nbShot)
+        hole.text = context.getString(R.string.hole, item.hole.name, item.hole.order)
     }
 
 }
